7875950c1d25d7cfa1d23c2829469b4111400a41
Mariano <marianogiardinamonti1@gmail.com>
2025-08-04 15:51:49 -0300

    Terminado balances y template home

---
+++ src/main/java/com/app_remiseria/AppGestorRemiseriaSpring/controller/CerrarSemanaController.java
@@ -0,0 +1,4 @@
+package com.app_remiseria.AppGestorRemiseriaSpring.controller;
+
+public class CerrarSemanaController {
+}
---
+++ src/main/java/com/app_remiseria/AppGestorRemiseriaSpring/dto/BalanceMensualDto.java
@@ -0,0 +1,4 @@
+package com.app_remiseria.AppGestorRemiseriaSpring.dto;
+
+public class BalanceMensualDto {
+}
